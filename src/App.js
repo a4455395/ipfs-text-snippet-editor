1fbb1acab3e952431b4257f65fee0d5a888a2d80
a4455395 <a4455395@gmail.com>
2018-10-04 09:52:34 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
+import history from './services/history'
+import HomePage from './components/Home'
+import ErrorPage from './components/ErrorPage'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+export default function App() {
+  return (
+    <Router history={history}>
+      <div>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route path="/error" component={ErrorPage} />
+          <Redirect to="/home" />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  )
 }
-
-export default App;
